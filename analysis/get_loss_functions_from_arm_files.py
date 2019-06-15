a07c2468603feed4ce25fa42524a121232e0d90a
crm15 <cristian.matache15@imperial.ac.uk>
2019-06-15 16:44:26 +0100

    get loss functions scripts

--- analysis/get_loss_functions_from_arm_files.py
+++ analysis/get_loss_functions_from_arm_files.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 import pickle
+import json
 
-OUTPUTS_DIR = "/vol/biomedic2/jpassera/cristian_param-tuning/outputs/hb+tpe+transfer+surv"
+OUTPUTS_DIR = "/vol/biomedic2/jpassera/cristian_param-tuning/outputs/hb+tpe"
+ONLY_OPTIMUMS = True
+OPTIMUM_LF_THRESH = 100
 
-print("Searching for result files")
+
+print(f"Searching for arm files in {OUTPUTS_DIR}")
 files = [f for f in Path(OUTPUTS_DIR).glob("**/loss_progress.model.pth")]
 print("Found files")
-print([f for f in files])
+[print(f) for f in files]
 
 res = {}
 for filepath in files:
@@ -14,17 +18,19 @@ for filepath in files:
         with open(filepath, "rb") as f:
             print(f"----- started {filepath}")
             loss_func = pickle.load(f)
-            res[str(filepath)] = loss_func
+            if ONLY_OPTIMUMS and len(loss_func) > OPTIMUM_LF_THRESH:
+                res[str(filepath)] = loss_func
             print(f"----- finished {filepath}")
     except Exception as e:
         print(f"----------------------file {filepath} failed")
         print(e)
 
-print({k:v for k, v in res.items()})
+res_info = sorted({k: (len(v), v[-1]) for k, v in res.items()}.items(), key=lambda kv: kv[1][0])
+print(json.dumps(res_info, indent=4))
 print()
 print({len(v) for k, v in res.items()})
 
 print("Pickling result")
-with open("loss_functions_from_arm.pkl", "wb") as f:
+with open("longest_loss_functions_from_arm_none.pkl", "wb") as f:
     pickle.dump(res, f)
 
--- analysis/get_loss_functions_from_result_files.py
+++ analysis/get_loss_functions_from_result_files.py
@@ -4,6 +4,7 @@ import json
 
 OUTPUTS_DIR = "/vol/biomedic2/jpassera/cristian_param-tuning/outputs/"
 TO_SEARCH = "**/results*"
+ONLY_OPTIMUMS = False
 
 print(f"Searching for {TO_SEARCH} result files in {OUTPUTS_DIR}")
 files = [f for f in Path(OUTPUTS_DIR).glob(TO_SEARCH)]
@@ -12,19 +13,27 @@ print("Found files")
 
 res = {}
 for filepath in files:
+    if "tpe" in str(filepath):
+        print(f"skipping tpe file {filepath}")
+        continue
     try:
         with open(filepath, "rb") as f:
             print(f"----- started {filepath}")
             optimum_evaluation, eval_history, checkpoints = pickle.load(f)
-            res[str(filepath)] = [(e.loss_history, e.arm) for e, g in eval_history]
-            # res[str(filepath)] = [(optimum_evaluation[0].loss_history, optimum_evaluation[0].arm)]
+            if not ONLY_OPTIMUMS:
+                # res[str(filepath)] = [(e.loss_history, e.arm) for e, g in eval_history if len(e.loss_history) > 290]
+                res[str(filepath)] = [e.loss_history for e, g in eval_history if len(e.loss_history) > 250]
+                # res[str(filepath)] = [e.loss_history for e, g in eval_history if len(e.loss_history) > 290][0]
+            else:
+                res[str(filepath)] = [(optimum_evaluation[0].loss_history, optimum_evaluation[0].arm)]
             print(f"----- finished {filepath}")
     except Exception as e:
         print(f"----------------------file {filepath} failed")
         print(e)
 
-print(json.dumps({k: [len(lf) for lf, _ in v] for k, v in res.items()},indent=4, sort_keys=True))
+# print(json.dumps({k: [len(lf) for lf, _ in v] for k, v in res.items()},indent=4, sort_keys=True))
+print(json.dumps({k: [len(lf) for lf in v] for k, v in res.items()},indent=4, sort_keys=True))
 
 print("Pickling result")
-with open("best_loss_functions_cifar_hb.pkl", "wb") as f:
+with open("long_loss_functions_cifar_hb.pkl", "wb") as f:
     pickle.dump(res, f)
