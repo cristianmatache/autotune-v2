08fdaf79c038ced3a18dafb79e5dac2523125dfc
Cristian Matache <cristianmatache@hotmail.com>
2020-06-01 01:24:12 +0100

    using typevar in flatten

--- autotune/util/datastructures.py
+++ autotune/util/datastructures.py
@@ -1,5 +1,7 @@
-from typing import List, Any
+from typing import List, TypeVar
+
+T = TypeVar('T')
 
 
-def flatten(list_of_lists: List[List[Any]]) -> List[Any]:
+def flatten(list_of_lists: List[List[T]]) -> List[T]:
     return [val for sublist in list_of_lists for val in sublist]
